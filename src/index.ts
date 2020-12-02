03230ca1a586cbacdd1e88ab37de09ba7730ddd6
Axe <shane369@gmail.com>
2020-12-02 15:04:02 -0600

    allow use other vue instance (#3)
    * allow use other vue instance
    
    * strong type return value;
    bumpup version
    
    * rename composition function return type interface

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-import { computed, getCurrentInstance } from '@vue/composition-api'
+import { computed, getCurrentInstance, WritableComputedRef } from '@vue/composition-api'
+import { VueConstructor } from 'vue/types/umd'
 
 let i18nInstance: VueI18n | undefined
 
-export function createI18n(options?: VueI18n.I18nOptions) {
-  Vue.use(VueI18n)
+export function createI18n(options?: VueI18n.I18nOptions, vue: VueConstructor = Vue): VueI18n {
+  vue.use(VueI18n)
   i18nInstance = new VueI18n(options)
 
   return i18nInstance
 }
 
-export function useI18n() {
+export interface Composer {
+  locale: WritableComputedRef<string>
+  t: typeof VueI18n.prototype.t
+  tc: typeof VueI18n.prototype.tc
+  te: typeof VueI18n.prototype.te
+  d: typeof VueI18n.prototype.d
+  n: typeof VueI18n.prototype.n
+}
+
+export function useI18n(): Composer {
   if (!i18nInstance)
     throw new Error('vue-i18n not initialized')
 
