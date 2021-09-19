9be85e3c7c2277a1d44b6630b8e59052e71f1d6d
kazuya kawaguchi <kawakazu80@gmail.com>
2021-09-19 18:19:54 +0900

    feat: back-port from vue-i18n-next

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,73 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import { computed, getCurrentInstance } from '@vue/composition-api'
 import { VueConstructor } from 'vue/types/umd'
+import { isBoolean, assign } from '@intlify/shared'
 import type { WritableComputedRef } from '@vue/composition-api'
+import type {
+  I18nOptions,
+  ComposerOptions,
+  I18n,
+  I18nMode
+} from '@intlify/vue-i18n-core'
 
-let i18nInstance: VueI18n | undefined
+export function createI18n(options: I18nOptions = {}): VueI18n & I18n {
+  const legacyMode = isBoolean(options.legacy) ? options.legacy : true
+  const i18nMode: I18nMode = legacyMode ? 'legacy' : 'composition'
 
-export function createI18n(options?: VueI18n.I18nOptions): VueI18n {
-  i18nInstance = new VueI18n(options)
+  const i18n = new VueI18n(
+    convertToLegacyI18nOptions(options, i18nMode)
+  ) as VueI18n & I18n
 
-  return i18nInstance
+  const _i18n = i18n as any
+  _i18n.mode = i18nMode
+  _i18n.global = _i18n
+
+  return i18n
+}
+
+function convertToLegacyI18nOptions(
+  options: I18nOptions,
+  mode: I18nMode
+): VueI18n.I18nOptions {
+  let legacyOptions: VueI18n.I18nOptions = {}
+  legacyOptions.locale = options.locale
+  legacyOptions.fallbackLocale = options.fallbackLocale
+  legacyOptions.messages = options.messages
+  legacyOptions.dateTimeFormats = options.datetimeFormats // from vue-i18n@v9.x
+  legacyOptions.numberFormats = options.numberFormats
+  legacyOptions.missing = options.missing
+  legacyOptions.modifiers = options.modifiers as VueI18n.Modifiers
+  legacyOptions.fallbackRoot = options.fallbackRoot
+
+  if (mode === 'legacy') {
+    legacyOptions = assign(legacyOptions, options)
+  } else {
+    // convert from composer options
+    const composerOptions = options as ComposerOptions
+    legacyOptions.sync = composerOptions.inheritLocale
+
+    legacyOptions.silentFallbackWarn = isBoolean(composerOptions.fallbackWarn)
+      ? !composerOptions.fallbackWarn
+      : composerOptions.fallbackWarn
+    legacyOptions.silentTranslationWarn = isBoolean(composerOptions.missingWarn)
+      ? !composerOptions.missingWarn
+      : composerOptions.missingWarn
+    // prettier-ignore
+    legacyOptions.formatFallbackMessages = isBoolean(composerOptions.fallbackFormat)
+      ? !composerOptions.fallbackFormat
+      : composerOptions.fallbackFormat
+    legacyOptions.pluralizationRules = composerOptions.pluralRules
+    legacyOptions.warnHtmlInMessage = isBoolean(composerOptions.warnHtmlMessage)
+      ? composerOptions.warnHtmlMessage
+        ? 'warn'
+        : 'off'
+      : 'off'
+    legacyOptions.postTranslation =
+      composerOptions.postTranslation as VueI18n.PostTranslationHandler
+    legacyOptions.escapeParameterHtml = composerOptions.escapeParameter
+  }
+
+  return legacyOptions
 }
 
 export interface Composer {
@@ -22,15 +81,14 @@ export interface Composer {
 }
 
 export function useI18n(): Composer {
-  if (!i18nInstance) throw new Error('vue-i18n not initialized')
-
-  const i18n = i18nInstance
-
   const instance = getCurrentInstance()
   const vm =
     instance?.proxy ||
     (instance as unknown as InstanceType<VueConstructor>) ||
     new Vue({})
+
+  const i18n = vm.$i18n as VueI18n & I18n
+  if (!i18n) throw new Error('vue-i18n not initialized')
 
   const locale = computed({
     get() {
--- test/index.test.ts
+++ test/index.test.ts
@@ -3,30 +3,165 @@
  */
 import CompositionAPI, { nextTick } from '@vue/composition-api'
 import { createLocalVue, mount } from '@vue/test-utils'
+import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import { createI18n, useI18n } from '../src/index'
 
-const container = document.createElement('div')
-document.body.appendChild(container)
+let localVue
 
 beforeEach(() => {
-  container.innerHTML = ''
+  localVue = createLocalVue()
+  localVue.use(VueI18n)
 })
 
-test('createI18n', () => {
-  const localVue = createLocalVue()
-  localVue.use(VueI18n)
-  const i18n = createI18n({
-    locale: 'en',
-    fallbackLocale: 'en',
-    messages: {
-      en: { hello: 'Hello' },
-      ja: { hello: 'こんにちは' }
-    }
+afterEach(() => {
+  localVue = null
+})
+
+describe('createI18n', () => {
+  describe('legacy mode', () => {
+    test('basic', () => {
+      const i18n = createI18n({
+        legacy: true,
+        locale: 'en-US',
+        fallbackLocale: 'en-US',
+        messages: {
+          'en-US': { hello: 'Hello' },
+          'ja-JP': { hello: 'こんにちは' }
+        },
+        datetimeFormats: {
+          'en-US': {
+            short: {
+              year: 'numeric',
+              month: '2-digit',
+              day: '2-digit',
+              hour: '2-digit',
+              minute: '2-digit',
+              timeZone: 'America/New_York'
+            }
+          },
+          'ja-JP': {
+            long: {
+              year: 'numeric',
+              month: '2-digit',
+              day: '2-digit',
+              hour: '2-digit',
+              minute: '2-digit',
+              second: '2-digit',
+              timeZone: 'Asia/Tokyo'
+            },
+            short: {
+              year: 'numeric',
+              month: '2-digit',
+              day: '2-digit',
+              hour: '2-digit',
+              minute: '2-digit',
+              timeZone: 'Asia/Tokyo'
+            }
+          }
+        },
+        numberFormats: {
+          'en-US': {
+            currency: {
+              style: 'currency',
+              currency: 'USD',
+              currencyDisplay: 'symbol'
+            },
+            decimal: {
+              style: 'decimal',
+              useGrouping: false
+            }
+          },
+          'ja-JP': {
+            currency: {
+              style: 'currency',
+              currency: 'JPY'
+            },
+            numeric: {
+              style: 'decimal',
+              useGrouping: false
+            },
+            percent: {
+              style: 'percent',
+              useGrouping: false
+            }
+          }
+        }
+      })
+      expect(i18n.global).toEqual(i18n) // back-port from vue-i18n-next
+      expect(i18n.mode).toEqual('legacy') // back-port from vue-i18n-next
+      expect(i18n.locale).toEqual('en-US')
+      expect(i18n.fallbackLocale).toEqual('en-US')
+      expect(i18n.t('hello')).toEqual('Hello')
+      expect(
+        i18n.d(new Date(Date.UTC(2012, 11, 20, 3, 0, 0)), 'short', 'ja-JP')
+      ).toEqual('2012/12/20 12:00')
+      expect(i18n.n(0.99, 'percent', 'ja-JP')).toEqual('99%')
+    })
+
+    test('legacy default value', () => {
+      const i18n = createI18n({ legacy: true })
+      expect(i18n.locale).toEqual('en-US')
+      expect(i18n.fallbackLocale).toEqual('en-US')
+      expect(i18n.messages).toEqual({})
+      expect(i18n.dateTimeFormats).toEqual({})
+      expect(i18n.numberFormats).toEqual({})
+      expect(i18n.missing).toEqual(null)
+      expect(i18n.pluralizationRules).toEqual({})
+      expect(i18n.postTranslation).toEqual(null)
+      expect(i18n.preserveDirectiveContent).toEqual(false)
+      expect(i18n.silentFallbackWarn).toEqual(false)
+      expect(i18n.silentTranslationWarn).toEqual(false)
+      expect(i18n.formatFallbackMessages).toEqual(false)
+      expect(i18n.warnHtmlInMessage).toEqual('off')
+      expect((i18n as any)._escapeParameterHtml).toEqual(false)
+      expect((i18n as any)._modifiers).toEqual({})
+      expect((i18n as any)._componentInstanceCreatedListener).toEqual(null)
+    })
   })
-  expect(i18n.locale).toBe('en')
-  expect(i18n.fallbackLocale).toBe('en')
-  expect(i18n.t('hello')).toBe('Hello')
+
+  describe('composition mode', () => {
+    test.todo('basic')
+
+    test('composition default value', () => {
+      const i18n = createI18n({ legacy: false })
+      expect(i18n.locale).toEqual('en-US')
+      expect(i18n.fallbackLocale).toEqual('en-US')
+      expect(i18n.messages).toEqual({})
+      expect(i18n.dateTimeFormats).toEqual({})
+      expect(i18n.numberFormats).toEqual({})
+      expect(i18n.missing).toEqual(null)
+      expect(i18n.pluralizationRules).toEqual({})
+      expect(i18n.postTranslation).toEqual(null)
+      expect(i18n.preserveDirectiveContent).toEqual(false)
+      expect(i18n.silentFallbackWarn).toEqual(false)
+      expect(i18n.silentTranslationWarn).toEqual(false)
+      expect(i18n.formatFallbackMessages).toEqual(false)
+      expect(i18n.warnHtmlInMessage).toEqual('off')
+      expect((i18n as any)._escapeParameterHtml).toEqual(false)
+      expect((i18n as any)._modifiers).toEqual({})
+      expect((i18n as any)._componentInstanceCreatedListener).toEqual(null)
+    })
+
+    test('composition specify value', () => {
+      const pluralRules = { en: () => 0 }
+      const i18n = createI18n({
+        legacy: false,
+        inheritLocale: false,
+        fallbackWarn: true,
+        missingWarn: true,
+        fallbackFormat: true,
+        warnHtmlMessage: true,
+        pluralRules
+      })
+      expect(i18n.pluralizationRules).toEqual(pluralRules)
+      expect(i18n.silentFallbackWarn).toEqual(false)
+      expect(i18n.silentTranslationWarn).toEqual(false)
+      expect(i18n.formatFallbackMessages).toEqual(false)
+      expect(i18n.warnHtmlInMessage).toEqual('warn')
+      expect((i18n as any)._sync).toEqual(false)
+    })
+  })
 })
 
 /*
@@ -58,5 +193,30 @@ test('useI18n', async () => {
 
   await nextTick()
   expect(wrapper.text()).toEqual('Hello')
+})
+*/
+
+/*
+test('mount', async () => {
+  const localVue = createLocalVue()
+  localVue.use(VueI18n)
+
+  const i18n = new VueI18n({
+    locale: 'en',
+    messages: {
+      en: { hello: 'hello' },
+      ja: { hello: 'こんにちは' }
+    }
+  })
+
+  const wrapper = mount(
+    {
+      template: `<p>{{ $t('hello') }}</p>`
+    },
+    { localVue, i18n }
+  )
+
+  await Vue.nextTick()
+  expect(wrapper.text()).toEqual('hello')
 })
 */
